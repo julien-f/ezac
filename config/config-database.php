95c6c64ca75499535a2bd405e24fefe946e19ee9
Julien Fontanet <julien.fontanet@isonoe.net>
2012-04-26 16:10:35 +0200

    Enables UTF8 for MySQL.

--- config/config-database.php
+++ config/config-database.php
@@ -23,3 +23,5 @@
 
 $db_link = mysql_connect($sql_server, $user, $password) or die("Impossible de se connecter a la base");
 mysql_select_db($database, $db_link) or die( "Unable to select database");
+
+mysql_set_charset('utf8', $db_link);
